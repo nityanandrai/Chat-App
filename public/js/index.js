33f2de250cfb8df87c333ef50e5ba574c1142f9c
nityanandrai <nityanandtop@gmail.com>
2018-07-01 12:21:04 +0530

    added messaging feature

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  // socket.emit('createMessage', {
-  //   from: 'negative feedback',
-  //   text: 'Cant amplify the signal  '
-  // });
-
 });
 
 socket.on('disconnect', function () {
@@ -16,4 +10,26 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('New message arrived ', message);
+
+  var li = jQuery('<li></li>');
+  li.text(message.from+': '+message.text);
+  jQuery('#messages').append(li);
+});
+
+// socket.emit('createMessage', {
+//   from: 'Jeny',
+//   text: 'Jeny funked you'
+// }, function(data) {
+//   console.log('Got it Jeny ', data);
+// });
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function() {
+
+  });
 });
--- server/server.js
+++ server/server.js
@@ -19,9 +19,10 @@ io.on('connection',(socket) => {
 
   socket.broadcast.emit('newMessage', generateMessage('admin', 'New user joined'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('Message created by clinet arrived ', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server');
   });
 
   socket.on('disconnect', () => {
